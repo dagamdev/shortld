d83dec68e40e7a6bca629f4ed0024cf3b57115f1
Dagadev <gacub20ru@gmail.com>
2024-02-19 16:42:59 -0600

    Add user data logic & Add redirect root path

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,34 @@
 import { defineMiddleware } from 'astro:middleware'
 import { getSession } from 'auth-astro/server'
+import { Prisma } from './lib/prisma'
 
-export const onRequest = defineMiddleware(async ({ locals, request }, next) => {
-  console.log('middleware')
-  locals.session = await getSession(request)
+export const onRequest = defineMiddleware(async ({ locals, request, redirect, url }, next) => {
+  const session = await getSession(request)
+  locals.session = session
+
+  if (typeof session?.user?.email === 'string') {
+    try {
+      const user = await Prisma.user.findUnique({
+        where: {
+          email: session.user.email
+        }
+      })
+
+      if (user === null) {
+        const newUser = await Prisma.user.create({
+          data: {
+            name: session.user.name,
+            email: session.user.email
+          }
+        })
+        locals.user = newUser
+      } else locals.user = user
+    } catch (error) {
+      locals.user = null
+    }
+  }
+
+  if (url.pathname === '/dashboard' && session === null) return redirect('/')
 
   return await next()
 })
