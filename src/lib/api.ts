76b67aab67dc7a2d675894e07ca8ab10554182aa
Dagadev <gacub20ru@gmail.com>
2024-02-19 20:01:12 -0600

    Add createJsonRes function

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,11 +1,31 @@
+const headers = {
+  'Content-Type': 'application/json'
+}
+
 export function handleError (error: unknown) {
   return new Response(JSON.stringify({
-    message: error instanceof Error ? error.message : 'Have you made a mistake when making the request?'
+    error: error instanceof Error ? error.message : 'Have you made a mistake when making the request?'
   }), {
     status: 400,
     statusText: 'bad request',
-    headers: {
-      'Content-Type': 'application/json'
-    }
+    headers
   })
+}
+
+export function createJsonRes (body: any, init?: ResponseInit) {
+  if (init !== undefined) {
+    init.headers = headers
+  }
+  return new Response(JSON.stringify(body), init)
+}
+
+export function generateCode () {
+  let code = ''
+
+  for (let i = 0; i < 6; i++) {
+    const randomChar = (Math.floor(Math.random() * 21)).toString(20)
+    code += randomChar
+  }
+
+  return code
 }
