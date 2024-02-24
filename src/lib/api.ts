58a5336f03c091db7d69945044c1c8d64fa733e1
Dagadev <gacub20ru@gmail.com>
2024-02-23 22:38:58 -0600

    Remove customFetch function

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -61,16 +61,3 @@ export async function getCode () {
 
   return code
 }
-
-export async function customFetch (path: string, init?: RequestInit) {
-  const response = await fetch(`${location.origin}/api/${path}`, init)
-  const contenType = response.headers.get('Content-Type')
-
-  if (contenType !== 'application/json') {
-    return {
-      error: 'The response type is not json'
-    }
-  }
-
-  return await response.json()
-}
