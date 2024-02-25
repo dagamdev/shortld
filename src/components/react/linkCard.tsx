01b1a64ae834414839ea90b9fe9b31bc6746c863
Dagadev <gacub20ru@gmail.com>
2024-02-25 13:20:13 -0600

    Update styles & Import copy svg icons

--- src/components/react/linkCard.tsx
+++ src/components/react/linkCard.tsx
@@ -1,41 +1,7 @@
 import { useState } from 'react'
 import type { Link } from '@prisma/client'
-
-const copyIcon = (
-  <svg xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    strokeWidth="1.5"
-    stroke="currentColor"
-    fill="none"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  >
-    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-    <path d="M7 7m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z" />
-    <path d="M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1" />
-  </svg>
-)
-
-const copyCheckIcon = (
-  <svg className='text-green-500'
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    strokeWidth="1.5"
-    stroke="currentColor"
-    fill="none"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  >
-    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-    <path d="M7 7m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z" />
-    <path d="M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1" />
-    <path d="M11 14l2 2l4 -4" />
-  </svg>
-)
+import CopyIcon from '@/icons/react/copy'
+import CopyCheckIcon from '@/icons/react/copyCheck'
 
 export default function LinkCard ({ link }: {
   link: Link
@@ -67,7 +33,7 @@ export default function LinkCard ({ link }: {
           <em className='block ml-1 truncate'>{link.url}</em>
         </p>
       </a>
-      <button className='p-1.5 flex rounded-md bg-zinc-400/60 dark:bg-zinc-500/60' onClick={handleClick}>{copied ? copyCheckIcon : copyIcon}</button>
+      <button className='p-1.5 flex rounded-md hover:bg-zinc-400/60 dark:hover:bg-zinc-500/60' onClick={handleClick}>{copied ? <CopyCheckIcon className='stroke-green-500' /> : <CopyIcon />}</button>
     </li>
   )
 }
